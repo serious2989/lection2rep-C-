4e6ee211df5487e6c2b4a517b7a73131ec5dceea
Dmitriy Vasilyev <serious2989@gmail.com>
2022-10-03 17:23:40 +0300

    finished lection2

--- Program.cs
+++ Program.cs
@@ -1,17 +1,16 @@
 ﻿void FillArray(int[] collection)
-
 {
     int length = collection.Length;
     int index = 0;
     while (index < length)
-{
-    collection[index] = new Random() .Next(1,10);
-    index = index++;
-
-}
+ {
+    collection[index] = new Random().Next(1,10);
+    index++;
+ }
 }
 
 void PrintArray(int[]col)
+
 {
     int count = col.Length;
     int position = 0;
@@ -23,7 +22,30 @@ void PrintArray(int[]col)
 
 }
 
+int Indexof(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}     
 int[] array = new int [10];
 
 FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
 PrintArray(array);
+Console.WriteLine();
+
+int pos = Indexof(array, 444);
+Console.WriteLine(pos);
