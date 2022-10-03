a30522b50b599bbf4640d31052036c1ea5a4653d
Dmitriy Vasilyev <serious2989@gmail.com>
2022-10-03 16:33:50 +0300

    new commut

--- Program.cs
+++ Program.cs
@@ -1,17 +1,29 @@
-﻿int[] array = {1,12,31,4,15,16,17,18};
+﻿void FillArray(int[] collection)
 
-int n = array.Length;
-int find = 18;
-
-int index = 0;
-
-while (index < n)
 {
-    if(array[index] == find)
-    {
-      Console.WriteLine (index);
-    }
-    
-    index++;
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+{
+    collection[index] = new Random() .Next(1,10);
+    index = index++;
+
+}
 }
 
+void PrintArray(int[]col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+}
+
+int[] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
