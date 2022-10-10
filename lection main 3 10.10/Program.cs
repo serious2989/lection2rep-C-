2053e956bde230d5fe79976c56f336ab333a7ff2
Dmitriy Vasilyev <serious2989@gmail.com>
2022-10-10 10:35:19 +0300

    Написали два метода

--- lection main 3 10.10/Program.cs
+++ lection main 3 10.10/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вид 1. Фунции
+
+void Method1 ()
+
+{
+Console.WriteLine("Автор..");
+}
+
+//Method1() - зеленый чтобы работал второй метод 
+
+// Вид 2
+void Method2(string msq)
+
+{
+    Console.WriteLine(msq);
+}
+
+//Method2("Текст сообщения"); пока что убрал
+Console.Clear();
+void Method21(string msq, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msq);
+        i++;
+    }
+}
+
+//Method21(msq:"Текст сообщения",count: 4);
+//Method21(count: 4, msq:"NEW TEKST");
+
+//3 Вид
+
+
+
+ 
